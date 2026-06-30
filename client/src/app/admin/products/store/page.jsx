7fe556f9aea46dd85560f86c866951fa87131f6a
Kader1680 <ouldhenniabaghdad@gmail.com>
2026-06-30 23:27:56 +0100

    add new

--- client/src/app/admin/products/store/page.jsx
+++ client/src/app/admin/products/store/page.jsx
@@ -1,208 +1,208 @@
-"use client";
-import Link from "next/link";
-import { useEffect } from "react";
-import { useState } from "react";
+// "use client";
+// import Link from "next/link";
+// import { useEffect } from "react";
+// import { useState } from "react";
 
-export default function AddProductPage() {
-  const [form, setForm] = useState({
-    name: "",
-    description: "",
-    price: "",
-    stock: "",
-    image: null,
-    active: true,
-    category_id: "",
-  });
-  const [loading, setLoading] = useState(false);
-  const [message, setMessage] = useState("");
+// export default function AddProductPage() {
+//   const [form, setForm] = useState({
+//     name: "",
+//     description: "",
+//     price: "",
+//     stock: "",
+//     image: null,
+//     active: true,
+//     category_id: "",
+//   });
+//   const [loading, setLoading] = useState(false);
+//   const [message, setMessage] = useState("");
 
-  const handleChange = (e) => {
-    const { name, value, files, type, checked } = e.target;
-    setForm((prev) => ({
-      ...prev,
-      [name]:
-        type === "file" ? files[0] : type === "checkbox" ? checked : value,
-    }));
-  };
+//   const handleChange = (e) => {
+//     const { name, value, files, type, checked } = e.target;
+//     setForm((prev) => ({
+//       ...prev,
+//       [name]:
+//         type === "file" ? files[0] : type === "checkbox" ? checked : value,
+//     }));
+//   };
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    setLoading(true);
-    setMessage("");
+//   const handleSubmit = async (e) => {
+//     e.preventDefault();
+//     setLoading(true);
+//     setMessage("");
 
-    const formData = new FormData();
-    formData.append("name", form.name);
-    formData.append("description", form.description);
-    formData.append("price", form.price);
-    formData.append("stock", form.stock);
-    formData.append("category_id", form.category_id);
-    formData.append("active", form.active ? 1 : 0);
-    if (form.image) formData.append("image", form.image);
+//     const formData = new FormData();
+//     formData.append("name", form.name);
+//     formData.append("description", form.description);
+//     formData.append("price", form.price);
+//     formData.append("stock", form.stock);
+//     formData.append("category_id", form.category_id);
+//     formData.append("active", form.active ? 1 : 0);
+//     if (form.image) formData.append("image", form.image);
 
-    try {
-      const res = await fetch("http://localhost:8000/api/product", {
-        method: "POST",
-        body: formData,
-      });
-
-
-      setMessage("Product added successfully!");
-      setForm({
-        name: "",
-        description: "",
-        price: "",
-        stock: "",
-        image: null,
-        active: true,
-        category_id: "",
-      });
-      // redirect to admin panel
-
-      window.location.href = "/admin/products/index";
-    } catch (err) {
-      console.error(err);
-      setMessage("Failed to add product");
-    } finally {
-      setLoading(false);
-    }
-  };
+//     try {
+//       const res = await fetch("http://localhost:8000/api/product", {
+//         method: "POST",
+//         body: formData,
+//       });
 
 
-   const [categories, setCategories] = useState([]);
+//       setMessage("Product added successfully!");
+//       setForm({
+//         name: "",
+//         description: "",
+//         price: "",
+//         stock: "",
+//         image: null,
+//         active: true,
+//         category_id: "",
+//       });
+//       // redirect to admin panel
+
+//       window.location.href = "/admin/products/index";
+//     } catch (err) {
+//       console.error(err);
+//       setMessage("Failed to add product");
+//     } finally {
+//       setLoading(false);
+//     }
+//   };
+
+
+//    const [categories, setCategories] = useState([]);
   
-    useEffect(() => {
-      fetch("http://localhost:8000/api/categories")
-        .then((res) => res.json())
-        .then((data) => setCategories(data));
+//     useEffect(() => {
+//       fetch("http://localhost:8000/api/categories")
+//         .then((res) => res.json())
+//         .then((data) => setCategories(data));
        
-    }, []);
+//     }, []);
   
 
 
-  return (
-    <div className="p-6 mx-auto max-w-xl">
-      <h2 className="text-2xl sm:text-3xl font-bold text-slate-900 mb-6 text-center">
-        Add New Product
-      </h2>
+//   return (
+//     <div className="p-6 mx-auto max-w-xl">
+//       <h2 className="text-2xl sm:text-3xl font-bold text-slate-900 mb-6 text-center">
+//         Add New Product
+//       </h2>
 
-      {/* button to get retun admin panel */}
+//       {/* button to get retun admin panel */}
 
-      <button>
-        <Link to ="/admin/dashboard" className="text-blue-600 hover:underline mb-4 inline-block"  >
-        Back to admin
-        </Link>
-      </button>
+//       <button>
+//         <Link to ="/admin/dashboard" className="text-blue-600 hover:underline mb-4 inline-block"  >
+//         Back to admin
+//         </Link>
+//       </button>
 
-      <form
-        onSubmit={handleSubmit}
-        className="bg-white shadow-md rounded-lg p-6 space-y-4 border"
-      >
-        <div>
-          <label className="block text-gray-700 font-medium mb-1">Name</label>
-          <input
-            type="text"
-            name="name"
-            value={form.name}
-            onChange={handleChange}
-            required
-            className="w-full border border-gray-300 rounded-md p-2"
-          />
-        </div>
+//       <form
+//         onSubmit={handleSubmit}
+//         className="bg-white shadow-md rounded-lg p-6 space-y-4 border"
+//       >
+//         <div>
+//           <label className="block text-gray-700 font-medium mb-1">Name</label>
+//           <input
+//             type="text"
+//             name="name"
+//             value={form.name}
+//             onChange={handleChange}
+//             required
+//             className="w-full border border-gray-300 rounded-md p-2"
+//           />
+//         </div>
 
-        <div>
-          <label className="block text-gray-700 font-medium mb-1">
-            Description
-          </label>
-          <textarea
-            name="description"
-            value={form.description}
-            onChange={handleChange}
-            required
-            className="w-full border border-gray-300 rounded-md p-2 h-24"
-          />
-        </div>
+//         <div>
+//           <label className="block text-gray-700 font-medium mb-1">
+//             Description
+//           </label>
+//           <textarea
+//             name="description"
+//             value={form.description}
+//             onChange={handleChange}
+//             required
+//             className="w-full border border-gray-300 rounded-md p-2 h-24"
+//           />
+//         </div>
 
-        <div>
-          <label className="block text-gray-700 font-medium mb-1">Price</label>
-          <input
-            type="number"
-            name="price"
-            value={form.price}
-            onChange={handleChange}
-            required
-            className="w-full border border-gray-300 rounded-md p-2"
-          />
-        </div>
+//         <div>
+//           <label className="block text-gray-700 font-medium mb-1">Price</label>
+//           <input
+//             type="number"
+//             name="price"
+//             value={form.price}
+//             onChange={handleChange}
+//             required
+//             className="w-full border border-gray-300 rounded-md p-2"
+//           />
+//         </div>
 
-        <div>
-          <label className="block text-gray-700 font-medium mb-1">Stock</label>
-          <input
-            type="number"
-            name="stock"
-            value={form.stock}
-            onChange={handleChange}
-            required
-            className="w-full border border-gray-300 rounded-md p-2"
-          />
-        </div>
+//         <div>
+//           <label className="block text-gray-700 font-medium mb-1">Stock</label>
+//           <input
+//             type="number"
+//             name="stock"
+//             value={form.stock}
+//             onChange={handleChange}
+//             required
+//             className="w-full border border-gray-300 rounded-md p-2"
+//           />
+//         </div>
 
-        <div>
-          <label className="block text-gray-700 font-medium mb-1">Image</label>
-          <input
-            type="file"
-            name="image"
-            accept="image/*"
-            onChange={handleChange}
-            className="w-full text-gray-600"
-          />
-        </div>
+//         <div>
+//           <label className="block text-gray-700 font-medium mb-1">Image</label>
+//           <input
+//             type="file"
+//             name="image"
+//             accept="image/*"
+//             onChange={handleChange}
+//             className="w-full text-gray-600"
+//           />
+//         </div>
 
-        <div>
-          <label  className="block text-gray-700 font-medium mb-1">Category</label>
-          <select
-            name="category_id"
-            value={form.category_id || ""}
-            onChange={handleChange}
-            className="w-full border border-gray-300 rounded-md p-2"
-          >
-            <option value="">Select Category</option>
-            {categories.map((cat) => (
-              <option key={cat.id} value={cat.id}>{cat.name}</option>
-            ))}
-          </select>
-        </div>
-
-
-        <div className="flex items-center space-x-2">
-          <input
-            type="checkbox"
-            name="active"
-            checked={form.active}
-            onChange={handleChange}
-          />
-          <label className="text-gray-700 font-medium">Active</label>
-        </div>
+//         <div>
+//           <label  className="block text-gray-700 font-medium mb-1">Category</label>
+//           <select
+//             name="category_id"
+//             value={form.category_id || ""}
+//             onChange={handleChange}
+//             className="w-full border border-gray-300 rounded-md p-2"
+//           >
+//             <option value="">Select Category</option>
+//             {categories.map((cat) => (
+//               <option key={cat.id} value={cat.id}>{cat.name}</option>
+//             ))}
+//           </select>
+//         </div>
 
 
+//         <div className="flex items-center space-x-2">
+//           <input
+//             type="checkbox"
+//             name="active"
+//             checked={form.active}
+//             onChange={handleChange}
+//           />
+//           <label className="text-gray-700 font-medium">Active</label>
+//         </div>
 
-        <button
-          type="submit"
-          disabled={loading}
-          className="w-full bg-blue-600 hover:bg-blue-700 text-white py-2 rounded-md"
-        >
-          {loading ? "Adding..." : "Add Product"}
-        </button>
 
-        {message && (
-          <p
-            className={`text-center mt-2 ${
-              message.startsWith("✅") ? "text-green-600" : "text-red-600"
-            }`}
-          >
-            {message}
-          </p>
-        )}
-      </form>
-    </div>
-  );
-}
+
+//         <button
+//           type="submit"
+//           disabled={loading}
+//           className="w-full bg-blue-600 hover:bg-blue-700 text-white py-2 rounded-md"
+//         >
+//           {loading ? "Adding..." : "Add Product"}
+//         </button>
+
+//         {message && (
+//           <p
+//             className={`text-center mt-2 ${
+//               message.startsWith("✅") ? "text-green-600" : "text-red-600"
+//             }`}
+//           >
+//             {message}
+//           </p>
+//         )}
+//       </form>
+//     </div>
+//   );
+// }
